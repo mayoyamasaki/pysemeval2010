ec5e3c9ccee786b9d940d2d864979e358ca48184
mayo-ya <mayo.yamasaki@gmail.com>
2017-08-26 17:02:40 +0900

    [Add] annotation and persistence

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 
 
 ## Requirements
-Download ```TEST_FILE_FULL.TXT``` and ```TRAIN_FILE.TXT``` from SemEval-2010 website.  
-And save these files in this project's data directiory.
-You can download these files at [](http://semeval2.fbk.eu/semeval2.php?location=data)
+- SemEval 2010 Task 8 (https://docs.google.com/document/d/1QO_CnmvNRnYwNWu1-QCAeR5ToQYkXUqFeAJbdEhsq7w/preview 
+)
+  - Download ```TEST_FILE_FULL.TXT``` and ```TRAIN_FILE.TXT``` from SemEval-2010 website.  
+  - And save these files in this project's data directiory.
+
+## Make pickle file
+```
+python3 src/task8.py
+```
--- src/task8.py
+++ src/task8.py
@@ -1,5 +1,11 @@
 import sys
+import pickle
 from html.parser import HTMLParser
+
+from pycorenlp import StanfordCoreNLP
+
+
+CNLP = StanfordCoreNLP('http://localhost:9000')
 
 
 class EntityParser(HTMLParser):
@@ -46,6 +52,34 @@ def parse_sentence(s):
     return p.get_data()
 
 
+def annotate(e1, e2, s):
+    res = CNLP.annotate(s, properties={
+        #TODO 'annotators': 'tokenize,pos,depparse,ner',
+        'annotators': 'tokenize',
+        'outputFormat': 'json'
+    })
+
+    tokens = res['tokens']
+    words = []
+    _e1 = []
+    _e2 = []
+    for i, token in enumerate(tokens):
+        words.append(token['word'])
+        b = token['characterOffsetBegin']
+        e = token['characterOffsetEnd']
+
+        if (e1[0] <= b and e <= e1[1]+1) or (b <= e1[0] and e1[1]+1 <= e):
+            _e1.append(i)
+        if (e2[0] <= b and e <= e2[1]+1) or (b <= e2[0] and e2[1]+1 <= e):
+            _e2.append(i)
+
+    #print(s, ' : ', s[e1[0]:e1[1]+1], s[e2[0]:e2[1]+1])
+    #print(_e1, _e2)
+    #print(words, ' : ', words[_e1[0]:_e1[-1]+1], words[_e2[0]:_e2[-1]+1])
+
+    return (_e1, _e2, words)
+
+
 def split_relation_and_direction(r):
     O = 'Other'
     if r == O:
@@ -54,18 +88,27 @@ def split_relation_and_direction(r):
         return  r[:-7], r[-7:]
 
 
-def load_dataset(rows, split=False):
+def load_dataset(rows, split=True):
+    data = []
+    target = []
     rows = iter(rows)
     while True:
         try:
             sentnce_ann = next(rows).rstrip('\n').split('\t')[-1].strip('"')
         except StopIteration:
-            return
+            break
         e1, e2, sentence =  parse_sentence(sentnce_ann)
         relation = next(rows).rstrip('\n')
         next(rows) # Comment
         next(rows) # Blank line
-        print(e1, e2, sentence, split_relation_and_direction(relation))
+
+        data.append((e1, e2, sentence))
+        if split:
+            relation, direction = split_relation_and_direction(relation)
+            target.append((relation, direction))
+        else:
+            target.append(relation)
+    return data, target
 
 
 if __name__ == '__main__':
@@ -73,7 +116,14 @@ if __name__ == '__main__':
     TEST_FILE = 'data/TEST_FILE_FULL.TXT'
 
     with open(TRAIN_FILE, 'r', encoding='utf-8') as fd:
-        load_dataset(fd)
+        train_data, train_target = load_dataset(fd)
+    train_data = [annotate(e1, e2, s) for e1, e2, s in train_data]
+    with open('result/task8_train.pickle', 'wb') as fd:
+        pickle.dump((train_data, train_target), fd)
 
     with open(TEST_FILE, 'r', encoding='utf-8') as fd:
-        load_dataset(fd)
+        test_data, test_target = load_dataset(fd)
+    test_data = [annotate(e1, e2, s) for e1, e2, s in test_data]
+    with open('result/task8_test.pickle', 'wb') as fd:
+        pickle.dump((test_data, test_target), fd)
+
